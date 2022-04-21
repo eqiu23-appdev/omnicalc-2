bac5be888f50e79f3328350d61814e6514b61923
eqiu23 <eqiu@chicagobooth.edu>
2022-04-21 20:36:09 +0000

    addition done

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,7 +1,14 @@
 class MathController < ApplicationController
   def addition
-
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first + @second
     render({:template => "math_templates/add_results.html.erb"})
   end
+
+  def add_form
+    render({:template => "math_templates/addition_form.html.erb"})
+  end
+
 
 end
