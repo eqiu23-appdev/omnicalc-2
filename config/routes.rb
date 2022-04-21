7e8b3ba1f6a51ba107578684c433430185ba9acf
eqiu23 <eqiu@chicagobooth.edu>
2022-04-21 23:13:25 +0000

    divide done, 100%

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get("/wizard_multiply", {:controller => "math" , :action => "multiplication"})
   get("/multiply", {:controller => "math" , :action => "multiply_form"})
 
+  get("/wizard_divide", {:controller => "math" , :action => "division"})
+  get("/divide", {:controller => "math" , :action => "divide_form"})
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
